b8154e5766a4fede16eeca5cbf9dba1e64bb7a33
MowlandCodes <mowlandgaming@gmail.com>
2026-01-12 15:36:03 +0700

    testing connection to Database

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/loadenv.php';
+loadEnv(__DIR__ . '/.env');
+
 require __DIR__ . '/connection.php';
+
+try {
+    $conn = Database::getInstance();
+    $db = $conn->getDatabase('dbBasdat');
+
+    $collection = $db->getCollection('mahasiswa');
+
+    $count = $collection->countDocuments();
+    echo "<br>Jumlah data: $count";
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit;
+}
